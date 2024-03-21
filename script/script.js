72483a11029ddaf883f46c273503756f16e408bb
Onyinyechukwu Adesanya <yinadesanya@gmail.com>
2024-03-21 18:39:39 +0100

    pushing once again Thursday

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector("#mobile-menu");
-const mobile_nav = document.querySelector(".main-nav");
+const mobile_nav = document.querySelector("#mobile-nav");
 const header = document.querySelector(".header");
+const currentYear = document.querySelector(".year");
 
 // LOADING ANIMATION
 window.addEventListener("load", () => {
@@ -16,4 +17,29 @@ window.addEventListener("load", () => {
 menu.addEventListener("click", () => {
   menu.classList.toggle("is-active");
   mobile_nav.classList.toggle("nav-open");
+  mobile_nav.classList.toggle("hide-navbar");
 });
+
+// SWIPER CAROUSEL
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// SETTING THE CORRECT YEAR
+const getCurrentYear = new Date().getFullYear();
+currentYear.innerHTML = getCurrentYear;
